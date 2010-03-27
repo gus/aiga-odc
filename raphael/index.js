7fea2d48de83ed93c0c5dbf68524c4f72bc3ab48
Justin Knowlden <gus@gusg.us>
2010-03-27 12:11:16 -0500

    Updating with Raphael examples

---
+++ raphael/index.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  Raphael.fn.triangle = function(x, y, size) {
+    p1 = "L" + (x + size / 2) + " " + (y + size);
+    p2 = "L" + (x - size / 2) + " " + (y + size);
+    p3 = "L" + x + " " + y;
+    return this.path("M" + x + " " + y + p1 + p2 + p3);
+  };
+
+  var rotator = function(event) {
+    this.attr( {"rotation": 0});
+    this.animate( {"rotation": 7200}, 10000, ">");
+  };
+
+  var bg = Raphael(document.getElementById("here"), 800, 600);
+
+  bg.triangle(300, 0, 100).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
+  bg.triangle(200, 50, 100).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
+  bg.triangle(200, 150, 50).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
+  bg.triangle(325, 125, 50).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
+};
