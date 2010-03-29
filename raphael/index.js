68530b3aa94fcdbc6285a2d6693a59a1d714a249
Justin Knowlden <gus@gusg.us>
2010-03-29 18:19:37 -0500

    Got some circle stuff working, but not exactly the way I want

--- raphael/index.js
+++ raphael/index.js
@@ -1,20 +1,14 @@
-window.onload = function() {
-  Raphael.fn.triangle = function(x, y, size) {
-    p1 = "L" + (x + size / 2) + " " + (y + size);
-    p2 = "L" + (x - size / 2) + " " + (y + size);
-    p3 = "L" + x + " " + y;
-    return this.path("M" + x + " " + y + p1 + p2 + p3);
-  };
-
-  var rotator = function(event) {
-    this.attr( {"rotation": 0});
-    this.animate( {"rotation": 7200}, 10000, ">");
-  };
-
-  var bg = Raphael(document.getElementById("example"), 800, 400);
-
-  bg.triangle(300, 0, 100).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
-  bg.triangle(200, 50, 100).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
-  bg.triangle(200, 150, 50).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
-  bg.triangle(325, 125, 50).attr({"fill": "yellow", "stroke": 0}).mouseover(rotator);
+Raphael.fn.triangle = function(x, y, size) {
+  var path = ["M", x, y];
+  path = path.concat(["L", (x + size / 2), (y + size)]);
+  path = path.concat(["L", (x - size / 2), (y + size)]);
+  return this.path(path.concat(["z"]).join(" "));
 };
+
+var Aiga = { fx: {}};
+
+Aiga.fx.rotator = function(event) {
+  this.attr( {"rotation": 0});
+  this.animate( {"rotation": 7200}, 10000, ">");
+};
+
