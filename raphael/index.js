250b9a21b1f11eaea4aba7273bd86a0b66d95c42
Justin Knowlden <gus@gusg.us>
2010-03-29 23:00:42 -0500

    Some pulsate refactoring

--- raphael/index.js
+++ raphael/index.js
@@ -26,19 +26,15 @@ Aiga.fx.rotator = function(event) {
   this.animate({"rotation": 7200}, 10000, ">");
 };
 
-// Make a shape pulsate between 50% or 200% it's original size as the response to an event.
+// Handle pulsate as the response to an event
+Aiga.fx.pulsator = function(event) { Aiga.fx.pulsate(this); };
+
+// Make a shape pulsate between 50% or 200% it's original size
 // Will not pulsate again until scale is back to 1
-Aiga.fx.pulsator = function(event) {
-  var toPulsate = this, scale = toPulsate.attr("scale");
+Aiga.fx.pulsate = function(toPulsate) {
+  var scale = toPulsate.attr("scale");
   if (scale.x == 1 && scale.y == 1) {
     toPulsate.animate({"scale": (Aiga.math.tails() ? "2" : "0.5")}, 500);
     setTimeout(function() { toPulsate.animate({"scale": 1}, 500); }, 1000);
   }
-};
-
-// Make a shape pulsate between 50% and 200% it's original size, indefinitely
-Aiga.fx.pulsate = function(toPulsate, enlarge) {
-  enlarge = enlarge == undefined ? true : enlarge;
-  toPulsate.animate({"scale": enlarge ? 2 : 0.5}, 5000);
-  setTimeout(function() { Aiga.fx.pulsate(toPulsate, !enlarge); }, 5000)
 };
